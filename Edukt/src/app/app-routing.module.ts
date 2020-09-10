a79d0a7dc10d41db58fea1632f94623d2688b300
Erick Llumiquinga <eam.llumiquinga@yavirac.edu.ec>
2020-09-10 01:58:36 -0500

    changes completed

--- Edukt/src/app/app-routing.module.ts
+++ Edukt/src/app/app-routing.module.ts
@@ -15,11 +15,14 @@ const routes: Routes = [
 
   { path: 'profesores/lista', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./teachers/list-teachers/list-teachers.module').then(m => m.ListTeachersModule)},
   { path: 'profesores/nuevo', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./teachers/new-teacher/new-teacher.module').then(m => m.NewTeacherModule)},
-  { path: 'profesores/perfil/editar', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./teachers/edit-teacher/edit-teacher.module').then(m => m.EditTeacherModule)},
-  { path: 'course', loadChildren: () => import('./course/course.module').then(m => m.CourseModule) },
+  { path: 'profesores/perfil/editar/:id', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./teachers/edit-teacher/edit-teacher.module').then(m => m.EditTeacherModule)},
+  
+  { path: 'cursos/lista', loadChildren: () => import('./course/list-course/list-course.module').then(m => m.ListCourseModule) },
+  { path: 'cursos/nuevo', loadChildren: () => import('./course/new-course/new-course.module').then(m => m.NewCourseModule) },
+  { path: 'cursos/editar/:id', loadChildren: () => import('./course/edit-course/edit-course.module').then(m => m.EditCourseModule) },
+
   { path: 'new-homework', loadChildren: () => import('./homework/new-homework/new-homework.module').then(m => m.NewHomeworkModule) },
-  { path: 'list-homework', loadChildren: () => import('./homework/list-homework/list-homework.module').then(m => m.ListHomeworkModule) }
-];
+  { path: 'list-homework', loadChildren: () => import('./homework/list-homework/list-homework.module').then(m => m.ListHomeworkModule) },
 
   { path: 'clases/lista', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./classes/list-class/list-class.module').then(m => m.ListClassModule)},
   { path: 'clases/nuevo', /*canActivate: [ TokenGuard ],*/ loadChildren: () => import('./classes/new-class/new-class.module').then(m => m.NewClassModule)},
